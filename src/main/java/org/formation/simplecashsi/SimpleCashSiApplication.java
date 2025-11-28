60a77e1343a1c7bb7c9d067658afdb651763a794
firiti <safia.firiti05@gmail.com>
2025-11-28 09:47:54 +0100

    Initial project structure and dependencies setup

---
+++ src/main/java/org/formation/simplecashsi/SimpleCashSiApplication.java
@@ -0,0 +1,13 @@
+package org.formation.simplecashsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCashSiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleCashSiApplication.class, args);
+    }
+
+}
